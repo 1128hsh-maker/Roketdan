d93310a63ce3be7df746af0fcec598aa91662cbd
HanSeungHo <hshansh@naver.com>
2026-03-20 16:35:10 +0900

    feat: BackGround 및 기본 이미지 카메라 세팅

---
+++ RoketdanProject/Assets/02_Scripts/FitCameraToBackgroundHeight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FitCameraToBackgroundHeight : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer backgroundRenderer;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        Apply();
+    }
+
+    private void Apply()
+    {
+        if (backgroundRenderer == null)
+        {
+            Debug.LogWarning("Background Renderer가 연결되지 않았습니다.");
+            return;
+        }
+
+        cam.orthographic = true;
+
+        float bgHeight = backgroundRenderer.bounds.size.y;
+        cam.orthographicSize = bgHeight * 0.5f;
+
+        Vector3 center = backgroundRenderer.bounds.center;
+        transform.position = new Vector3(center.x, center.y, transform.position.z);
+    }
+}
